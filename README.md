fc52fb7de0a42fd3f4ca6407e02d58e562f8b88b
Sudip Kundu <sudipkundu999@gmail.com>
2023-10-10 09:16:30 +0530

    Release v1.1.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Hey there! I'm Sudip Kundu and this is my first npm package!
+## Hey there! I'm [Sudip Kundu](https://www.sudipkundu.com) & this is my first npm package!
 
 Run this in your terminal
 
--- index.js
+++ index.js
@@ -2,20 +2,20 @@
 
 function logDetails() {
   const message = "Hey there! I'm Sudip Kundu, a UI Developer at Flipkart!";
-  const twitter = "https://twitter.com/sudipkundu999";
-  const github = "https://github.com/sudipkundu999";
-  const linkedin = "https://www.linkedin.com/in/sudip-kundu";
-  const portfolio = "https://www.sudipkundu.com";
-    const colorfulBox = `    \x1b[38;5;51m+--------------------------------------------------------------+
+  const twitter = 'https://twitter.com/sudipkundu999';
+  const github = 'https://github.com/sudipkundu999';
+  const linkedin = 'https://linkedin.com/in/sudip-kundu';
+  const portfolio = 'https://sudipkundu.com';
+  const colorfulBox = `    \x1b[38;5;51m+--------------------------------------------------------------+
     |   \x1b[38;5;105m${message}\x1b[38;5;51m    |
     +--------------------------------------------------------------+
-    | \x1b[38;5;93mPortfolio:\x1b[0m  \x1b[38;5;39m${portfolio}                       \x1b[38;5;51m|
+    | \x1b[38;5;93mPortfolio:\x1b[0m  \x1b[38;5;39m${portfolio}                           \x1b[38;5;51m|
     | \x1b[38;5;93mGithub:\x1b[0m     \x1b[38;5;39m${github}                 \x1b[38;5;51m|
     | \x1b[38;5;93mTwitter:\x1b[0m    \x1b[38;5;39m${twitter}                \x1b[38;5;51m|
-    | \x1b[38;5;93mLinkedIn:\x1b[0m   \x1b[38;5;39m${linkedin}          \x1b[38;5;51m|
+    | \x1b[38;5;93mLinkedIn:\x1b[0m   \x1b[38;5;39m${linkedin}              \x1b[38;5;51m|
     +--------------------------------------------------------------+\x1b[0m`;
-  
-    console.log(colorfulBox);
-  }
+
+  console.log(colorfulBox);
+}
 
 logDetails();
